84e36fddcc7e16176ef992c1350dfe4e41cc0717
Renee Knox <renee5.0@outlook.com>
2019-08-30 14:13:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,10 +43,23 @@ def monopoly_with_second_tier
     {  
     :pieces => 4,
     :names => {},
-    :rent_in_dollars => {
-    }
-      
+    :rent_in_dollars => {}
     }
      
   }
 end
+
+def monopoly_with_third_tier
+  monopoly = {
+    :railroads =>
+     {  
+    :pieces => 4,
+    :names => {},
+    :rent_in_dollars => {
+      :one_piece_owned = 25
+    }
+    }
+     
+  }
+end
+  }
